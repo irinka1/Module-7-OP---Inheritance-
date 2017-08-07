c93ef33b700c275e588bc57a1218ba1e37b55f83
Irinka <irenmast@gmail.com>
2017-08-07 17:32:40 +0300

    14

--- src/player/Playsong.java
+++ src/player/Playsong.java
@@ -1,16 +1,25 @@
+package player;
+
 public class Playsong {
 
     String songName;
+    final  int price;
 
 
 
-    public Playsong(String songName) {
+    public Playsong(String songName, int price) {
         this.songName = songName;
+        this.price = price;
 
     }
     public void playSong() {
         System.out.println("Playing: " + songName);
 
-
     }
+
+    public int getPrice(){
+        return price;
+    }
+
+
 }
