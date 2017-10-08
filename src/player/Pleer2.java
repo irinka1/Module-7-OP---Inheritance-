8be4b67b481a9a10ed8359777f6d6cf677a95101
Ирина <irenmast@gmail.com>
2017-10-08 17:08:26 +0300

    Update Pleer2.java

--- src/player/Pleer2.java
+++ src/player/Pleer2.java
@@ -1,5 +1,11 @@
 package player;
+/*Player 2.
 
+Has a final price (specified in the designer) and a getter
+
+Has only 1 song (you can not declare this variable in the class of this player)
+
+playSong Trying to play a song gives an error to the console - "error".*/
 public class Pleer2 extends Playsong{
 
     public Pleer2(String songName, int price) {
