e1a9db4b4a765ff2f4f17fa669c76e433a5cf2db
Ирина <irenmast@gmail.com>
2017-10-08 17:08:02 +0300

    Update Pleer1.java

--- src/player/Pleer1.java
+++ src/player/Pleer1.java
@@ -1,5 +1,11 @@
 package player;
+/*Player 1.
 
+Has a final price (specified in the designer) and a getter
+
+Has only 1 song (you can not declare this variable in the class of this player)
+
+playSong Can play the song.*/
 public class Pleer1 extends Playsong {
 
 
