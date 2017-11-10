8e17dcda85357190dc25da1caab6a7020123ec4a
Ирина <irenmast@gmail.com>
2017-11-10 11:55:45 +0200

    Update Pleer5.java

--- src/player/Pleer5.java
+++ src/player/Pleer5.java
@@ -1,7 +1,7 @@
-/*Имеет final цену(задается в конструкторе) и геттер
-Имеет плейлист
-playSong Может проиграть первую песню
-playAllSongs может проиграть все песни с конца плейлиста в начало*/
+/*Has a final price (specified in the designer) and a getter
+Has a playlist
+playSong Can play the first song
+playAllSongs can play all songs from the end of the playlist to the beginning*/
 
 
 package player;
@@ -26,6 +26,6 @@ public class Pleer5 extends PlayAllSongs{
         playlist[0] = playlist[2];
         playlist[2] = n1;
 
-        System.out.println("Проигрываются все песни c конца в начало: " + Arrays.asList(playlist));
+        System.out.println("All songs are playing from the end to the beginning: " + Arrays.asList(playlist));
     }
 }
