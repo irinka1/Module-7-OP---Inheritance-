ed294e20e31f09c82a3ee2b507b187a307809008
Ирина <irenmast@gmail.com>
2017-11-10 11:29:33 +0200

    Update MAIN.java

--- src/MAIN.java
+++ src/MAIN.java
@@ -4,8 +4,8 @@ public class MAIN {
     public static void main(String[] args) {
 
 
-      Pleer1 pleer1 = new Pleer1("Песенка 1", 100);
-      Pleer2 pleer2 = new Pleer2("Песенка 2", 200);
+      Pleer1 pleer1 = new Pleer1("Song 1", 100);
+      Pleer2 pleer2 = new Pleer2("Song 2", 200);
       Pleer3 pleer3 = new Pleer3(new String[]{ "The best song", "Good song", "Super Song"}, 300);
       Pleer4 pleer4 = new Pleer4(new String[]{ "The best song", "Good song", "Super Song"}, 400);
       Pleer5 pleer5 = new Pleer5(new String[]{ "The best song", "Good song", "Super Song"}, 500);
